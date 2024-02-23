cc19a1bdd2d242bb1db54d1c624e6acc92801b29
kvasbulbas <shamonin.2002@mail.ru>
2024-02-23 19:33:31 +0700

    Copying a header file and cpp file

--- WidgetsQT28/widget.h
+++ WidgetsQT28/widget.h
@@ -2,6 +2,8 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QLineEdit>
+#include<QString>
 
 class Widget : public QWidget
 {
@@ -10,5 +12,24 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+    class Counter:public QLineEdit
+    {
+        Q_OBJECT
+    public:
+        Counter(const QString & contents, QWidget *parent=0):
+            QLineEdit(contents,parent){}
+    signals:
+        void tick_signal();
+    public slots:
+        void add_one()
+        {
+            QString str=text();
+            int r=str.toInt();
+            if (r!=0 && r%5 ==0) emit tick_signal();
+            r++;
+            str.setNum(r);
+            setText(str);
+        }
+    }
 };
 #endif // WIDGET_H
