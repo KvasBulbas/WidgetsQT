8f2b986d85f366df32dd4ffce00fe870b7db8af7
kvasbulbas <shamonin.2002@mail.ru>
2024-02-24 18:14:14 +0700

    Copying programm
    Area, Figura, Widget classes

--- WidgetsQT29/widget.cpp
+++ WidgetsQT29/widget.cpp
@@ -1,9 +1,18 @@
 #include "widget.h"
-
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+#include <QTextCodec>
+#include <QPushButton>
+#include <QVBoxLayout>
+Widget::Widget()
 {
-}
+
+    this->setWindowTitle("Обработка событий");
+    area = new Area( this );
+    btn = new QPushButton("Завершить",this );
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(area);
+    layout->addWidget(btn);
+    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+};
 
 Widget::~Widget()
 {
--- WidgetsQT29/widget.h
+++ WidgetsQT29/widget.h
@@ -2,13 +2,19 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include "area.h"
+#include<QPushButton>
 
 class Widget : public QWidget
 {
     Q_OBJECT
+protected:
+    QTextCodec *codec;
+    Area * area; // область отображения рисунка
+    QPushButton * btn;
 
 public:
-    Widget(QWidget *parent = nullptr);
+    Widget();
     ~Widget();
 };
 #endif // WIDGET_H
