e53427a5988888925a63898b080cb3f4dda8bc20
kvasbulbas <shamonin.2002@mail.ru>
2024-03-05 15:06:50 +0700

    Added new realisation for fucntion of connection

--- WidgetsQT29/widget.cpp
+++ WidgetsQT29/widget.cpp
@@ -5,14 +5,13 @@
 
 Widget::Widget()
 {
-
     this->setWindowTitle("Обработка событий");
     area = new Area(this );
     btn = new QPushButton("Завершить",this );
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(area);
     layout->addWidget(btn);
-    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+    connect(btn, QPushButton::clicked,this,QWidget::close);
 };
 
 Widget::~Widget()
